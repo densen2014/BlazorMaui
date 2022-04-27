8daff1e90d70f6b1193376dcd48eeb78660c0697
Alex Chow <zhouchuanglin@gmail.com>
2022-04-28 00:50:40 +0200

    BlazorMaui 升级RC2 , builder.Services.AddBlazorWebView(); =>  builder.Services.AddMauiBlazorWebView();
    csproj 一堆调整
    MainPage.xaml
    RegisterBlazorMauiWebView 去掉,

--- BlazorMaui/MauiProgram.cs
+++ BlazorMaui/MauiProgram.cs
@@ -11,14 +11,16 @@ namespace BlazorMaui
 
             var builder = MauiApp.CreateBuilder();
             builder
-                .RegisterBlazorMauiWebView()
                 .UseMauiApp<App>()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 });
 
-            builder.Services.AddBlazorWebView();
+            builder.Services.AddMauiBlazorWebView();
+#if DEBUG
+            builder.Services.AddBlazorWebViewDeveloperTools();
+#endif
             builder.Services.AddSharedExtensions();
 
             return builder.Build();
