e2bc4779828516be5158195f851e541bdcc6cbd3
Alex chow <zhouchuanglin@gmail.com>
2022-10-13 03:01:06 +0200

    BlazorPWA Configuration

---
+++ BlazorPWA/ConfigFake.cs
@@ -0,0 +1,9 @@
+﻿// ********************************** 
+// Densen Informatica 中讯科技 
+// 作者：Alex Chow
+// e-mail:zhouchuanglin@gmail.com 
+// **********************************
+
+public class ConfigFake
+{
+}
--- BlazorPWA/Program.cs
+++ BlazorPWA/Program.cs
@@ -1,11 +1,13 @@
 ﻿using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using BlazorShared;
+using Microsoft.Extensions.Configuration;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 
 builder.Services.AddSharedExtensions();
+builder.Configuration.AddUserSecrets<ConfigFake>();
 builder.Services.AddOcrExtensions(builder.Configuration["AzureCvKey"], builder.Configuration["AzureCvUrl"]);
 builder.Services.AddAIFormExtensions(builder.Configuration["AzureAiFormKey"], builder.Configuration["AzureAiFormUrl"]);
 await builder.Build().RunAsync();
