c71fb1058ebd22a0433a072eba33b80c53fb1517
Alex Chow <zhouchuanglin@gmail.com>
2022-12-04 14:48:59 +0100

    Platforms_Windows_SetTitle 动态设置窗口标题

---
+++ MauiApp7test/Pages/Index.razor.cs
@@ -0,0 +1,26 @@
+﻿// ********************************** 
+// Densen Informatica 中讯科技 
+// 作者：Alex Chow
+// e-mail:zhouchuanglin@gmail.com 
+// **********************************
+
+namespace MauiApp7test.Pages;
+
+public partial class Index
+{
+ 
+
+ 
+    protected override void OnAfterRender(bool firstRender)
+    {
+        if (firstRender)
+        {
+#if WINDOWS
+            WinUI.App.SetTitle("MauiApp7test - Index");
+#endif 
+        }
+    }
+  
+}
+
+
--- MauiApp7test/Platforms/Windows/App.xaml.cs
+++ MauiApp7test/Platforms/Windows/App.xaml.cs
@@ -4,7 +4,10 @@
 // e-mail:zhouchuanglin@gmail.com 
 // **********************************
 
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
+using WinRT.Interop;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -16,6 +19,9 @@ namespace MauiApp7test.WinUI
     /// </summary>
     public partial class App : MauiWinUIApplication
     {
+        public static object CurrentWindow;
+        public static AppWindow AppWindow;
+        
         /// <summary>
         /// Initializes the singleton application object.  This is the first line of authored code
         /// executed, and as such is the logical equivalent of main() or WinMain().
@@ -24,6 +30,19 @@ namespace MauiApp7test.WinUI
         {
             this.InitializeComponent();
         }
+        protected override void OnLaunched(LaunchActivatedEventArgs args)
+        {
+            base.OnLaunched(args);
+
+            CurrentWindow = Application.Windows[0].Handler?.PlatformView;
+            IntPtr _windowHandle = WindowNative.GetWindowHandle(CurrentWindow);
+            var windowId = Win32Interop.GetWindowIdFromWindow(_windowHandle);
+            AppWindow = AppWindow.GetFromWindowId(windowId);
+            
+            SetTitle("MauiApp7test");
+        }
+        public static void SetTitle(string title) => AppWindow.Title = title;
+
 
         protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
     }
