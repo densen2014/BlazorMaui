a13ef67703189fb8029d51b94acef7c479d26358
alex chow <8428709+densen2014@users.noreply.github.com>
2023-01-01 16:03:26 +0100

    ios 提示 can not save UniqueID

--- BlazorMauiLite/MainPage.xaml.cs
+++ BlazorMauiLite/MainPage.xaml.cs
@@ -4,6 +4,19 @@
 // e-mail:zhouchuanglin@gmail.com 
 // **********************************
 
+using LibraryShared;
+using Microsoft.AspNetCore.Components.WebView;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Platform;
+using System;
+using static Microsoft.Maui.ApplicationModel.Permissions;
+using System.IO;
+#if ANDROID
+using AndroidX.Activity;
+#elif WINDOWS
+using BlazorMaui.Platforms.Windows;
+#endif
+
 namespace BlazorMauiLite
 {
     public partial class MainPage : ContentPage
@@ -11,6 +24,46 @@ namespace BlazorMauiLite
         public MainPage()
         {
             InitializeComponent();
+
+            blazorWebView.BlazorWebViewInitialized += BlazorWebViewInitialized;
+            blazorWebView.BlazorWebViewInitializing += BlazorWebViewInitializing;
+        }
+
+        private void BlazorWebViewInitialized(object? sender, BlazorWebViewInitializedEventArgs e)
+        {
+#if ANDROID
+            if (e.WebView.Context?.GetActivity() is not ComponentActivity activity)
+            {
+                throw new InvalidOperationException($"The permission-managing WebChromeClient requires that the current activity be a '{nameof(ComponentActivity)}'.");
+            }
+
+            e.WebView.Settings.JavaScriptEnabled = true;
+            e.WebView.Settings.AllowFileAccess = true;
+            e.WebView.Settings.MediaPlaybackRequiresUserGesture = false;
+            e.WebView.Settings.SetGeolocationEnabled(true);
+            e.WebView.Settings.SetGeolocationDatabasePath(e.WebView.Context?.FilesDir?.Path);
+            e.WebView.SetWebChromeClient(new PermissionManagingBlazorWebChromeClient(e.WebView.WebChromeClient!, activity));
+#elif WINDOWS
+            var permissionHandler =
+#if HANDLE_WEBVIEW2_PERMISSIONS_SILENTLY
+            new SilentPermissionRequestHandler();
+#else
+                new DialogPermissionRequestHandler(e.WebView);
+#endif
+
+            e.WebView.CoreWebView2.PermissionRequested += permissionHandler.OnPermissionRequested;
+#elif IOS
+        //e.Configuration.AllowsInlineMediaPlayback = true;
+        //e.Configuration.MediaTypesRequiringUserActionForPlayback = WebKit.WKAudiovisualMediaTypes.None;
+#endif
+        }
+
+        private void BlazorWebViewInitializing(object? sender, BlazorWebViewInitializingEventArgs e)
+        {
+#if IOS || MACCATALYST                   
+            e.Configuration.AllowsInlineMediaPlayback = true;
+            e.Configuration.MediaTypesRequiringUserActionForPlayback = WebKit.WKAudiovisualMediaTypes.None;
+#endif
         }
     }
 }
--- BlazorShared/Interface/IPAddressManager.cs
+++ BlazorShared/Interface/IPAddressManager.cs
@@ -135,23 +135,30 @@ namespace Shared.DependencyServices
 #if (IOS || MACCATALYST)
         public string? GetIdentifier()
         {
-            var query = new SecRecord(SecKind.GenericPassword);
-            query.Service = NSBundle.MainBundle.BundleIdentifier;
-            query.Account = "UniqueID";
+            try{
+                var query = new SecRecord(SecKind.GenericPassword);
+                query.Service = NSBundle.MainBundle.BundleIdentifier;
+                query.Account = "UniqueID";
 
-            NSData? uniqueId = SecKeyChain.QueryAsData(query);
-            if (uniqueId == null)
-            {
-                query.ValueData = NSData.FromString(System.Guid.NewGuid().ToString());
-                var err = SecKeyChain.Add(query);
-                if (err != SecStatusCode.Success && err != SecStatusCode.DuplicateItem)
-                    throw new Exception("Cannot store Unique ID");
+                NSData? uniqueId = SecKeyChain.QueryAsData(query);
+                if (uniqueId == null)
+                {
+                    query.ValueData = NSData.FromString(System.Guid.NewGuid().ToString());
+                    var err = SecKeyChain.Add(query);
+                    if (err != SecStatusCode.Success && err != SecStatusCode.DuplicateItem)
+                        throw new Exception("Cannot store Unique ID");
 
-                return query.ValueData.ToString();
+                    return query.ValueData.ToString();
+                }
+                else
+                {
+                    return uniqueId.ToString();
+                }
+            
             }
-            else
+            catch (Exception)
             {
-                return uniqueId.ToString();
+                return "UnknowIdentifier";
             }
         }
 
