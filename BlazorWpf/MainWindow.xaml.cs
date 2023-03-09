b5b2db7b0c6b3ab47817db41ed7140c72c3b1328
Alex chow <zhouchuanglin@gmail.com>
2023-03-09 21:28:34 +0100

    wpf 工程 : iframe 里下载文件

--- BlazorWpf/MainWindow.xaml.cs
+++ BlazorWpf/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using BlazorShared;
 using Microsoft.AspNetCore.Components.WebView.Wpf;
+using Microsoft.Web.WebView2.Core;
 using Microsoft.Web.WebView2.Wpf;
 using System.IO;
 using System.Net.NetworkInformation;
@@ -12,10 +13,23 @@ namespace BlazorWpf
     /// </summary>
     public partial class MainWindow : Window
     {
+        protected string UploadPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "uploads");
+
         public MainWindow()
         {
             Resources.Add("services", Startup.Services);
             InitializeComponent();
+            blazorWebView.BlazorWebViewInitialized += BlazorWebViewInitialized;
+
+        }
+
+        void BlazorWebViewInitialized(object sender, EventArgs e)
+        {
+            //下载开始时引发 DownloadStarting，阻止默认下载
+            blazorWebView.WebView.CoreWebView2.DownloadStarting += CoreWebView2_DownloadStarting;
+
+            //指定下载保存位置
+            blazorWebView.WebView.CoreWebView2.Profile.DefaultDownloadFolderPath = UploadPath;
 
             ////[无依赖发布webview2程序] 固定版本运行时环境的方式来实现加载网页
             ////设置web用户文件夹 
@@ -28,9 +42,18 @@ namespace BlazorWpf
             //    BrowserExecutableFolder = browserExecutableFolder
             //};
             //mainBlazorWebView.WebView.CreationProperties = creationProperties;
-
         }
 
+        private void CoreWebView2_DownloadStarting(object sender, CoreWebView2DownloadStartingEventArgs e)
+        {
+            var downloadOperation = e.DownloadOperation;
+            string fileName = Path.GetFileName(e.ResultFilePath);
+            var filePath = Path.Combine(UploadPath, fileName);
+
+            //指定下载保存位置
+            e.ResultFilePath = filePath;
+            MessageBox.Show( $"下载文件完成 {fileName}", "提示");
+        }
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
             MessageBox.Show(
@@ -41,7 +64,7 @@ namespace BlazorWpf
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            mainBlazorWebView.WebView.CoreWebView2.ExecuteScriptAsync("showAlert()");
+            blazorWebView.WebView.CoreWebView2.ExecuteScriptAsync("showAlert()");
         }
     }
 }
