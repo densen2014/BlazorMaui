53100896aa10fb30573d123ef8755b18088b00bd
alex chow <8428709+densen2014@users.noreply.github.com>
2023-03-13 21:50:37 +0100

    Update MainPage.xaml.cs

--- MauiApp_PdfReader/MainPage.xaml.cs
+++ MauiApp_PdfReader/MainPage.xaml.cs
@@ -20,138 +20,141 @@ using WebKit;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 #endif
 
-namespace MauiApp_PdfReader
+namespace MauiApp_PdfReader;
+
+#if IOS || MACCATALYST
+//在 iPadOS 和 Mac Catalyst 上使用多窗口支持
+[Register("SceneDelegate")]
+#endif
+public partial class MainPage : ContentPage
 {
-    public partial class MainPage : ContentPage
+    protected string UploadPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "uploads");
+
+    public MainPage()
     {
-        protected string UploadPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "uploads");
+        InitializeComponent();
 
-        public MainPage()
+        blazorWebView.BlazorWebViewInitialized += BlazorWebViewInitialized;
+        blazorWebView.BlazorWebViewInitializing += BlazorWebViewInitializing;
+
+        blazorWebView.UrlLoading +=
+        (sender, urlLoadingEventArgs) =>
         {
-            InitializeComponent();
-
-            blazorWebView.BlazorWebViewInitialized += BlazorWebViewInitialized;
-            blazorWebView.BlazorWebViewInitializing += BlazorWebViewInitializing;
-
-            blazorWebView.UrlLoading +=
-            (sender, urlLoadingEventArgs) =>
+            if (urlLoadingEventArgs.Url.Host != "0.0.0.0")
             {
-                if (urlLoadingEventArgs.Url.Host != "0.0.0.0")
+                //外部链接WebView内打开,例如pdf浏览器
+                Console.WriteLine(urlLoadingEventArgs.Url);
+                urlLoadingEventArgs.UrlLoadingStrategy =
+                    UrlLoadingStrategy.OpenInWebView;
+
+                //拦截可处理 IOS || MACCATALYST 下载文件, 简单测试一下
+                if (urlLoadingEventArgs.Url.ToString().EndsWith(".exe"))
                 {
-                    //外部链接WebView内打开,例如pdf浏览器
-                    Console.WriteLine(urlLoadingEventArgs.Url);
-                    urlLoadingEventArgs.UrlLoadingStrategy =
-                        UrlLoadingStrategy.OpenInWebView;
-
-                    //拦截可处理 IOS || MACCATALYST 下载文件, 简单测试一下
-                    if (urlLoadingEventArgs.Url.ToString().EndsWith(".exe"))
-                    {
-                        Task.Run(async () => await DownloadAsync(urlLoadingEventArgs.Url));
-                    }
+                    Task.Run(async () => await DownloadAsync(urlLoadingEventArgs.Url));
                 }
-            };
+            }
+        };
 
-            //Window secondWindow = new Window(new MainPage());
-            //Application.Current.OpenWindow(secondWindow);
-        }
+        //Window secondWindow = new Window(new MainPage());
+        //Application.Current.OpenWindow(secondWindow);
+    }
 
-        private void BlazorWebViewInitialized(object? sender, BlazorWebViewInitializedEventArgs e)
-        {
+    private void BlazorWebViewInitialized(object? sender, BlazorWebViewInitializedEventArgs e)
+    {
 
 #if ANDROID
 
-            if (e.WebView.Context?.GetActivity() is not ComponentActivity activity)
-            {
-                throw new InvalidOperationException($"The permission-managing WebChromeClient requires that the current activity be a '{nameof(ComponentActivity)}'.");
-            }
+        if (e.WebView.Context?.GetActivity() is not ComponentActivity activity)
+        {
+            throw new InvalidOperationException($"The permission-managing WebChromeClient requires that the current activity be a '{nameof(ComponentActivity)}'.");
+        }
 
-            e.WebView.Settings.JavaScriptEnabled = true;
-            e.WebView.Settings.AllowFileAccess = true;
-            e.WebView.Settings.MediaPlaybackRequiresUserGesture = false;
-            e.WebView.Settings.SetGeolocationEnabled(true);
-            e.WebView.Settings.SetGeolocationDatabasePath(e.WebView.Context?.FilesDir?.Path);
-            e.WebView.Download +=(async (s,e)=> await WebView_DownloadAsync(s,e));
-            //e.WebView.SetWebChromeClient(new PermissionManagingBlazorWebChromeClient(e.WebView.WebChromeClient!, activity));
+        e.WebView.Settings.JavaScriptEnabled = true;
+        e.WebView.Settings.AllowFileAccess = true;
+        e.WebView.Settings.MediaPlaybackRequiresUserGesture = false;
+        e.WebView.Settings.SetGeolocationEnabled(true);
+        e.WebView.Settings.SetGeolocationDatabasePath(e.WebView.Context?.FilesDir?.Path);
+        e.WebView.Download +=(async (s,e)=> await WebView_DownloadAsync(s,e));
+        //e.WebView.SetWebChromeClient(new PermissionManagingBlazorWebChromeClient(e.WebView.WebChromeClient!, activity));
 #elif WINDOWS
-            e.WebView.CoreWebView2.DownloadStarting += (async (s, e) => await CoreWebView2_DownloadStartingAsync(s, e));
+        e.WebView.CoreWebView2.DownloadStarting += (async (s, e) => await CoreWebView2_DownloadStartingAsync(s, e));
 #elif IOS || MACCATALYST
-            e.WebView.NavigationDelegate = new NavigationDelegate();
+        e.WebView.NavigationDelegate = new NavigationDelegate();
 #endif
 
-        }
+    }
 
 
 #if WINDOWS
-        private async Task CoreWebView2_DownloadStartingAsync(object sender, CoreWebView2DownloadStartingEventArgs e)
-        {
-            var downloadOperation = e.DownloadOperation;
-            string fileName = Path.GetFileName(e.ResultFilePath);
-            var filePath = Path.Combine(UploadPath, fileName);
+    private async Task CoreWebView2_DownloadStartingAsync(object sender, CoreWebView2DownloadStartingEventArgs e)
+    {
+        var downloadOperation = e.DownloadOperation;
+        string fileName = Path.GetFileName(e.ResultFilePath);
+        var filePath = Path.Combine(UploadPath, fileName);
 
-            //指定下载保存位置
-            e.ResultFilePath = filePath;
-            await DisplayAlert("提示", $"下载文件完成 {fileName}", "OK");
-        }
+        //指定下载保存位置
+        e.ResultFilePath = filePath;
+        await DisplayAlert("提示", $"下载文件完成 {fileName}", "OK");
+    }
 #endif
 
 
 #if ANDROID
-        private async Task WebView_DownloadAsync(object sender, DownloadEventArgs e)
-        {
-            //attachment; filename=ndp48-web.exe; filename*=UTF-8''ndp48-web.exe
-            //var file = e.ContentDisposition;
-            Uri uri = new Uri(e.Url);
-            await DownloadAsync(e.Url);
-        }
+    private async Task WebView_DownloadAsync(object sender, DownloadEventArgs e)
+    {
+        //attachment; filename=ndp48-web.exe; filename*=UTF-8''ndp48-web.exe
+        //var file = e.ContentDisposition;
+        Uri uri = new Uri(e.Url);
+        await DownloadAsync(e.Url);
+    }
 #endif
 
-        private async Task DownloadAsync(string url)
-        {
-            Uri uri = new Uri(url);
-            await DownloadAsync(uri);
-        }
+    private async Task DownloadAsync(string url)
+    {
+        Uri uri = new Uri(url);
+        await DownloadAsync(uri);
+    }
 
-        private async Task DownloadAsync(Uri uri)
-        {
-            string fileName = Path.GetFileName(uri.LocalPath);
-            var httpClient = new HttpClient();
-            var filePath = Path.Combine(UploadPath, fileName);
-            byte[] fileBytes = await httpClient.GetByteArrayAsync(uri);
-            File.WriteAllBytes(filePath, fileBytes);
-            await DisplayAlert("提示", $"下载文件完成 {fileName}", "OK");
-        }
+    private async Task DownloadAsync(Uri uri)
+    {
+        string fileName = Path.GetFileName(uri.LocalPath);
+        var httpClient = new HttpClient();
+        var filePath = Path.Combine(UploadPath, fileName);
+        byte[] fileBytes = await httpClient.GetByteArrayAsync(uri);
+        File.WriteAllBytes(filePath, fileBytes);
+        await DisplayAlert("提示", $"下载文件完成 {fileName}", "OK");
+    }
 
-        private void BlazorWebViewInitializing(object? sender, BlazorWebViewInitializingEventArgs e)
-        {
+    private void BlazorWebViewInitializing(object? sender, BlazorWebViewInitializingEventArgs e)
+    {
 #if IOS || MACCATALYST
-            e.Configuration.AllowsInlineMediaPlayback = true;
-            e.Configuration.MediaTypesRequiringUserActionForPlayback = WebKit.WKAudiovisualMediaTypes.None;
-#endif
-        }
-
-
-#if IOS || MACCATALYST
-
-
-        public class NavigationDelegate : NSObject, IWKNavigationDelegate
-        {
-
-            [Export("webView:didFinishNavigation:")]
-
-            public async void DidFinishNavigation(WKWebView webView, WKNavigation navigation)
-
-            {
-
-                var content = await webView.EvaluateJavaScriptAsync("(function() { return (''+document.getElementsByTagName('html')[0].innerHTML+''); })();");
-
-                var html = FromObject(content);
-
-                Console.WriteLine((html.ToString()).Substring(0, 40));
-
-            }
-
-        }
-
+        e.Configuration.AllowsInlineMediaPlayback = true;
+        e.Configuration.MediaTypesRequiringUserActionForPlayback = WebKit.WKAudiovisualMediaTypes.None;
 #endif
     }
+
+
+#if IOS || MACCATALYST
+
+
+    public class NavigationDelegate : NSObject, IWKNavigationDelegate
+    {
+
+        [Export("webView:didFinishNavigation:")]
+
+        public async void DidFinishNavigation(WKWebView webView, WKNavigation navigation)
+
+        {
+
+            var content = await webView.EvaluateJavaScriptAsync("(function() { return (''+document.getElementsByTagName('html')[0].innerHTML+''); })();");
+
+            var html = FromObject(content);
+
+            Console.WriteLine((html.ToString()).Substring(0, 40));
+
+        }
+
+    }
+
+#endif
 }
