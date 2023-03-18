396798b2c203df45fb4dc4f5f24000ca571568ae
Alex chow <zhouchuanglin@gmail.com>
2023-03-18 21:41:56 +0100

    test

---
+++ TestLibrary/Class1.cs
@@ -0,0 +1,17 @@
+﻿// ********************************** 
+// Densen Informatica 中讯科技 
+// 作者：Alex Chow
+// e-mail:zhouchuanglin@gmail.com 
+// **********************************
+
+namespace TestLibrary
+{
+    public class Class1
+    {
+        public Class1()
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+    }
+}
