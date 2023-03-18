84cd429497cfa72f67362789a28875d5b0b5c31d
alex chow <8428709+densen2014@users.noreply.github.com>
2023-03-18 23:29:16 +0100

    s

--- TestLibrary/Class1.cs
+++ TestLibrary/Class1.cs
@@ -1,16 +1,17 @@
-﻿// ********************************** 
-// Densen Informatica 中讯科技 
+﻿// **********************************
+// Densen Informatica 中讯科技
 // 作者：Alex Chow
-// e-mail:zhouchuanglin@gmail.com 
+// e-mail:zhouchuanglin@gmail.com
 // **********************************
 
 namespace TestLibrary
 {
     public class Class1
     {
-        public Class1()
+        public static string Test()
         {
             Console.WriteLine("Hello World!");
+            return "xxx";
         }
 
     }
