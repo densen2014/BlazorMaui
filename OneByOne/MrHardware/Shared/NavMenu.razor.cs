e0be3388e3c9b568a2847e957bbddf8672748cdc
Alex chow <zhouchuanglin@gmail.com>
2023-04-26 17:35:31 +0200

    NavMenu 组件 依赖新 bb 包 7.5.7

--- OneByOne/MrHardware/Shared/NavMenu.razor.cs
+++ OneByOne/MrHardware/Shared/NavMenu.razor.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Components.Routing;
 
 namespace MrHardware.Shared;
 
-[JSModuleAutoLoader("menu", ModuleName = "Menu")]
 public partial class NavMenu
 {
     private IEnumerable<MenuItem> Menus { get; set; } = new List<MenuItem>
